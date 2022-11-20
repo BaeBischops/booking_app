1c1b2b93a11a6db351a187d202ad5c961cf2491c
BaeBischops <dr.bkzee@gmail.com>
2022-11-20 18:12:43 +0200

    update

--- Back_Ended_Framework/OOP_Booking_App/modules/sign_in.php
+++ Back_Ended_Framework/OOP_Booking_App/modules/sign_in.php
@@ -14,31 +14,49 @@ if (isset($_SESSION["authenticated"]))
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/logging.css">
+  <script src="https://kit.fontawesome.com/d6c47f4f0c.js" crossorigin="anonymous"></script>
   <title>Sign in</title>
 </head>
 <body>
-  <div class="container">
-    <div class="row">
-      <h3 class="mb-0 my-2">Property Rentals</h3>
-      <div><a href="index.php" class="btn btn-dark">Home</a></div>
-      <form class="form" role="form" autocomplete="off" id="login-form" method="post">
-        <div class="form-group">
-          <label for="loginEmail">Email <span class="red-asterisk">*</span></label>
-          <input type="text" class="form-control" id="loginEmail" name="loginEmail" placeholder="email address" required>
-        </div><br>
-        <div class="form-group">
-          <label for="loginPassword">Password <span class="red-asterisk">*</span></label>
-          <input type="password" class="form-control" id="loginPassword" name="loginPassword" placeholder="password" required>
-        </div><br>
-        <div class="form-group">
-          <input type="submit" class="btn btn-primary btn-md float-right" value="Sign in" name="loginSubmitBtn">
-        </div>
-      </form>
-      <div class="form-group">
-        <p>Not registered? <a href="register.php">Register here.</a></p>
-    </div>
+  <div class="form-group">
+    <a  href="index.php" class="btn btn-dark">Home</a>
+  </div>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
+
+        <div class="login100-pic js-tilt" data-tilt><img src="../imgs/img1.jpg" alt="IMG"></div>
+        <form class="login100-form validate-form" action="" method="post">
+          <span class="login100-form-title">Member Login</span>
+
+          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+            <input class="input100" type="text" name="email" placeholder="Email">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa fa-envelope" aria-hidden="true"></i> </span>
+          </div>
+
+          <div class="wrap-input100 validate-input" data-validate = "Password is required">
+            <input class="input100" type="password" name="pass" placeholder="Password">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa fa-lock" aria-hidden="true"></i> </span>
+          </div>
+            
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn">Login</button>
+          </div>
+
+          <div class="text-center p-t-12">
+            <span class="txt1">Forgot</span><a class="txt2" href="forgot.php">Username / Password?</a>
+          </div>
+
+          <div class="text-center p-t-136">
+            <a class="txt2" href="sign_Up.php">Create your Account</a>
+          </div>
+        </form>
+
+      </div>
     </div>
   </div>
                
--- Back_Ended_Framework/OOP_Booking_App/modules/sign_up.php
+++ Back_Ended_Framework/OOP_Booking_App/modules/sign_up.php
@@ -1,5 +1,4 @@
 <?php
-ob_start();
 session_start();
 
 if (isset($_SESSION["authenticated"]))
@@ -12,55 +11,63 @@ if (isset($_SESSION["authenticated"]))
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>Register</title>
-  </head>
-  <body class="bg-secondary">
-    <div class="container">
-      <h2>Property Rentals</h2>
-      <div class="row">
-        <h3 class="mb-0 my-2">Sign Up</h3>
-                        </div>
-                        <div class="form-group">
-                            <a  href="index.php" class="btn btn-dark">Home</a>
-                        </div>
-                        <div class="card-body">
-                            <form class="form" role="form" autocomplete="off" id="registration-form" method="post">
-                                <div class="form-group">
-                                    <label for="registrationFullName">Name<span class="red-asterisk">*</span></label>
-                                    <input type="text" class="form-control" id="registrationFullName" name="registrationFullName" placeholder="Full name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="registrationEmail">Email<span class="red-asterisk">*</span></label> 
-                                    <input type="email" class="form-control" id="registrationEmail" name="registrationEmail" placeholder="email@domain.com" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="registrationPassword">Password<span class="red-asterisk">*</span></label>
-                                    <input type="password" class="form-control" id="registrationPassword" name="registrationPassword" placeholder="password" title="At least 4 characters with letters and numbers" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="registrationPassword2">Confirm Password<span class="red-asterisk">*</span></label>
-                                    <input type="password" class="form-control" id="registrationPassword2" name="registrationPassword2" placeholder="Retype Password" required>
-                                </div>
-                                <div class="form-group">
-                                    <p>Already registered? <a href="sign-in.php">Sign in here.</a></p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary btn-md float-right" name="registerSubmitBtn" value="Submit">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/logging.css">
+  <script src="https://kit.fontawesome.com/d6c47f4f0c.js" crossorigin="anonymous"></script>
+  <title>Sign in</title>
+</head>
+<body>
+    <div class="form-group">
+        <a  href="index.php" class="btn btn-dark">Home</a>
     </div>
-    </div>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
 
-    <script src="js/form-submission.js"></script>
-  </body>
+        <div class="login100-pic js-tilt" data-tilt><img src="../imgs/img1.jpg" alt="IMG"></div>
+        <form class="login100-form validate-form" action="" method="post">
+          <span class="login100-form-title">Member Registration</span>
+
+          <div class="wrap-input100 validate-input" data-validate = "Valid Name is required">
+            <input class="input100" type="text" name="name" placeholder="First and Last Name">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa-regular fa-id-card" aria-hidden="true"></i> </span>
+          </div>
+
+          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: example@mail.xyz">
+            <input class="input100" type="text" name="email" placeholder="Email">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa fa-envelope" aria-hidden="true"></i> </span>
+          </div>
+
+          <div class="wrap-input100 validate-input" data-validate = "Password is required">
+            <input class="input100" type="password" name="pass" placeholder="Password">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa fa-lock" aria-hidden="true"></i> </span>
+          </div>
+
+          <div class="wrap-input100 validate-input" data-validate = "Password is required">
+            <input class="input100" type="password" name="pass" placeholder="Confirm Password">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100"> <i class="fa fa-lock" aria-hidden="true"></i> </span>
+          </div>
+            
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn">Sign Up</button>
+          </div>
+
+          <div class="text-center p-t-136">
+            <a class="txt2" href="sign_in.php">Already Have Account</a>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+               
+  <script src="js/form-submission.js"></script>
+</body>
 </html>
