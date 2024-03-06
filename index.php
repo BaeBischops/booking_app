fb647c37ba0cad542ddc60f1d6147270d94aefff
BaeBischops <dr.bkzee@gmail.com>
2024-03-07 00:07:33 +0200

    update

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- styling -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="style.css">
+    <title>Booking App</title>
+</head>
+<body>
+    <!-- the HEADER section -->
+    <header></header>
+
+    <!-- the MAIN section -->
+    <section class="main"></section>
+
+    <!-- the FOOTER section -->
+    <footer></footer>
+    <!-- sripting -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
