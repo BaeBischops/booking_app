48f0a755c050705322ea88e7a2493fa87c6e4e87
BaeBischops <dr.bkzee@gmail.com>
2024-03-28 19:29:28 +0200

    the onscroll

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menuBtn').onclick = () =>{
     navbar.classList.toggle('active');
- }
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+ }; 
+
