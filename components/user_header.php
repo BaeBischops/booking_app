eedae50800014bdbc9ce7756038b7d7ad914b8cd
BaeBischops <dr.bkzee@gmail.com>
2024-04-01 10:48:22 +0200

    update

---
+++ components/user_header.php
@@ -0,0 +1,15 @@
+<section class="header">
+    <div class="flex">
+        <a href="#home" class="logo">reservations.africa</a>
+        <a href="#availability" class="btn">check availability</a>
+        <div id="menuBtn" class="bi bi-list"></div>
+    </div>
+    <nav class="navBar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#reservations">reservations</a>
+        <a href="#galary">galary</a>
+        <a href="#contacts">contacts</a>
+        <a href="#signIn">sign in</a>
+    </nav>
+</section>
--- index.php
+++ index.php
@@ -118,21 +118,7 @@ if(isset($_POST['send'])){
 </head>
 <body>
     <!-- THE HEADER SECTION -->
-    <section class="header">
-        <div class="flex">
-            <a href="#home" class="logo">reservations.africa</a>
-            <a href="#availability" class="btn">check availability</a>
-            <div id="menuBtn" class="bi bi-list"></div>
-        </div>
-        <nav class="navBar">
-            <a href="#home">home</a>
-            <a href="#about">about</a>
-            <a href="#reservations">reservations</a>
-            <a href="#galary">galary</a>
-            <a href="#contacts">contacts</a>
-            <a href="#signIn">sign in</a>
-        </nav>
-    </section>
+    <?php include 'components/user_header.php'; ?>
 
     <!-- THE HOME SECTION -->
     <section class="home">
